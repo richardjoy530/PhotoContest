e5195f099c16e6c66d6c1bf921fe9a607f998d89
Richard Joy <richardjoy530@gmail.com>
2021-01-04 10:32:36 +0530

    routing added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,15 +2,37 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { LoginComponent } from './login/login.component';
+import { RootComponent } from './root/root.component';
+import { RouterModule, RouterOutlet } from '@angular/router';
 
+const route = [{
+  path: "",
+  redirectTo: "login",
+  pathMatch: 'full',
+},
+{
+  path: "login",
+  component: LoginComponent,
+  data: { animation: "login" }
+}, {
+  path: "**",
+  redirectTo: "login",
+  pathMatch: 'full',
+}]
 @NgModule({
   declarations: [
-    LoginComponent
+    LoginComponent,
+    RootComponent
   ],
   imports: [
+    RouterModule.forRoot(route),
     BrowserModule,
   ],
   providers: [],
-  bootstrap: [LoginComponent]
+  bootstrap: [RootComponent]
 })
-export class AppModule { }
+export class AppModule { 
+  prepareRoute(outlet: RouterOutlet) {
+    return outlet && outlet.activatedRouteData
+  }
+}
---
+++ src/app/root/root.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+import { RouterOutlet } from '@angular/router';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './root.component.html',
+  styleUrls: ['./root.component.sass']
+})
+export class RootComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+}
