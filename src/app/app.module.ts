8fe764b93e1b4ffd1392c0aa6e4d717f3d25a0d6
Richard Joy <richardjoy530@gmail.com>
2021-01-05 08:46:10 +0530

    firebase-storage photo upload working

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,25 +1,30 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
+import { AngularFireStorageModule } from '@angular/fire/storage';
 import { LoginComponent } from './components/login/login.component';
 import { RootComponent } from './components/root/root.component';
 import { RouterModule } from '@angular/router';
 import { AuthService } from './services/auth.service';
 import { GalleryComponent } from './components/gallery/gallery.component';
 import { route } from './Routes'
+import { environment } from 'src/environments/environment';
+import { AngularFireModule } from '@angular/fire';
 
 
 @NgModule({
   declarations: [
     LoginComponent,
     RootComponent,
-    GalleryComponent
+    GalleryComponent,
   ],
   imports: [
     RouterModule.forRoot(route),
     BrowserModule,
+    AngularFireModule.initializeApp(environment.firebaseConfig),
+    AngularFireStorageModule
   ],
-  providers: [AuthService],
+  providers: [AuthService  ],
   bootstrap: [RootComponent]
 })
 export class AppModule { }
