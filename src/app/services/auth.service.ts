b3099cf163eed685fb338cc35931eccdaa760156
Richard Joy <richardjoy530@gmail.com>
2021-01-11 08:51:43 +0530

    obtaining photoEntryId from firestore changed firesase to another env

--- src/app/services/auth.service.ts
+++ src/app/services/auth.service.ts
@@ -1,7 +1,9 @@
 import { Injectable } from '@angular/core';
 import { AngularFireAuth } from '@angular/fire/auth';
+import { AngularFirestore } from '@angular/fire/firestore';
 import { Router } from '@angular/router';
 import firebase from 'firebase/app';
+import { UserData } from '../model/UserData';
 
 @Injectable({
   providedIn: 'root'
@@ -9,13 +11,43 @@ import firebase from 'firebase/app';
 export class AuthService {
 
   currentUser: firebase.User | undefined
-  
-  constructor(public auth: AngularFireAuth, private route: Router) {
+  userData: UserData | undefined
+
+  constructor(public auth: AngularFireAuth, private route: Router, private firestore: AngularFirestore) {
     this.auth.user.subscribe((user: any) => this.currentUser = user)
     this.auth.authState.subscribe((user: any) => {
-      if (user) route.navigate(["gallery"])
+      if (user as firebase.User) {
+        route.navigate(["gallery"])
+        this.syncUserVotes()
+      }
       else route.navigate(["login"])
     })
+
+  }
+
+  syncUserVotes() {
+    var a = this.firestore.doc("users/" + this.currentUser?.uid).valueChanges()
+    a.subscribe({
+      next(x) { console.log('got value ' , x); },
+      error(err) { console.error('something wrong occurred: ' + err); },
+      complete() { console.log('done'); }
+    })
+
+    // this.firestore.doc("users/" + this.currentUser?.uid).valueChanges().subscribe((userd: any) => {
+    //   if (userd == undefined)
+    //     this.firestore.collection<UserData>('users').doc(this.currentUser?.uid).set({
+    //         first: "",
+    //         second: "",
+    //         third: ""
+    //     })
+    //   else
+    //     this.userData = userd
+    //   console.log(this.userData)
+    // })
+  }
+
+  updateUserVotes(userVotes: UserData) {
+    this.firestore.collection<UserData>('users').doc(this.currentUser?.uid).update(userVotes)
   }
 
   loginWithGoogle() {
@@ -29,7 +61,6 @@ export class AuthService {
   logout() {
     this.auth.signOut();
   }
-
 
 }
 
