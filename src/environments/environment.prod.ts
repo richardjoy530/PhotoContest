4af99024e0224851d7550c4b14f3b70039b810f0
Richard Joy <richardjoy530@gmail.com>
2021-05-30 08:46:53 +0530

    Changed environment variables

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,13 @@
 export const environment = {
-  production: true
+  production: true,
+  firebaseConfig: {
+    apiKey: 'AIzaSyBTJWJC-5nG0BkBmj9vjCuv3QsGjFZGM7A',
+    authDomain: 'photocontest-7d8c2.firebaseapp.com',
+    databaseURL: 'https://photocontest-7d8c2-default-rtdb.firebaseio.com',
+    projectId: 'photocontest-7d8c2',
+    storageBucket: 'photocontest-7d8c2.appspot.com',
+    messagingSenderId: '928025547165',
+    appId: '1:928025547165:web:f609a2b1699562ad249f74',
+    measurementId: 'G-GMKT16QBTF'
+  }
 };
