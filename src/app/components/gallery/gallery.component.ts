413ba1490e6b05c0f50b8b90d7d9cc0d7f29f87e
Richard Joy <richardjoy530@gmail.com>
2021-07-02 08:29:49 +0530

    Updated Results

--- src/app/components/gallery/gallery.component.ts
+++ src/app/components/gallery/gallery.component.ts
@@ -31,7 +31,7 @@ export class GalleryComponent {
 
   constructor(private firestore: AngularFirestore, private storage: AngularFireStorage, public authService: AuthService) {
     document.body.classList.remove("bg")
-    var theme = "Nostalgia" // Need to fix this one day
+    var theme = "Insects" // Need to fix this one day
     console.log(this.authService.config?.theme)
     if (this.authService.config) {
       console.log(this.authService)
