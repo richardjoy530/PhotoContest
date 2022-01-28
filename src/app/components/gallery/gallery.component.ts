fad91218668bc457d2642d6fb9102667ee3c1ac5
Richard Joy <richardjoy530@gmail.com>
2022-01-28 11:12:18 +0530

    Update results

--- src/app/components/gallery/gallery.component.ts
+++ src/app/components/gallery/gallery.component.ts
@@ -31,7 +31,7 @@ export class GalleryComponent {
 
   constructor(private firestore: AngularFirestore, private storage: AngularFireStorage, public authService: AuthService) {
     document.body.classList.remove("bg")
-    var theme = "Creative Photos" // Need to fix this one day
+    var theme = "Your Euphoria" // Need to fix this one day
     console.log(this.authService.config?.theme)
     if (this.authService.config) {
       console.log(this.authService)
